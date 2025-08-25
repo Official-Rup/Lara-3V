7ddd943d66237102c4731417832b6c0376992f72
Sreejit sarkar <107984412+Official-Rup@users.noreply.github.com>
2025-08-25 12:20:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
-DEV: process.env.DEV || "94779062397",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "918617845264",
+DEV: process.env.DEV || "918617845264",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
